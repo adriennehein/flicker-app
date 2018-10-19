944e90f67c936e649796133fae2ab772e38f1779
Adrienne Hein <adrienne.hein@gmail.com>
2018-10-19 16:18:49 -0700

    initial commit

---
+++ flickr.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('form').submit(function(e) {
+    e.preventDefault();
+    const flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    const filter = $('#filter').val();
+    const flickerOptions = {
+      tags: filter,
+      format: "json"
+    }
+    const displayPhotos = (data) => {
+      let photoGrid = '<div class="photo-grid">';
+      $.each(data.items, function(i, photo){
+        photoGrid +=
+        `<div class="photo">
+          <a href="${photo.link}">
+            <div class="image" style="background:url(${photo.media.m}) center center; background-size: cover;">
+            <div class="label">${photo.title}</div>
+            </div>
+          </a>
+        </div>`
+      })
+      photoGrid += '</div>'
+      $('#photos').html(photoGrid)
+    }
+    $.getJSON(flickerAPI, flickerOptions, displayPhotos)
+  });
+
+
+});
